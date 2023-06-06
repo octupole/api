ef316500bcada85f58322ce0919d89c2a549c5eb
octupole <33395395+octupole@users.noreply.github.com>
2023-06-06 16:45:04 +0200

    Bamby

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+const AboutPage = () =>{
+    return     (
+    <div>
+        <h1>about</h1>
+        <p> This is a test</p>
+        <p> This is a test</p>
+    </div>
+    );
+}
+export default AboutPage;
